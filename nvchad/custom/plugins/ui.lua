4f3bc1ed1a9454397ddac9dc721274cd49e172eb
shikr <cristobal.rod0907@gmail.com>
2023-08-03 13:59:10 -0600

    feat: add projects button in veil

--- nvchad/custom/plugins/ui.lua
+++ nvchad/custom/plugins/ui.lua
@@ -76,7 +76,55 @@ return {
   {
     'willothy/veil.nvim',
     lazy = false,
-    config = true,
+    config = function()
+      local builtin = require 'veil.builtin'
+
+      require('veil').setup {
+        sections = {
+          builtin.sections.animated(builtin.headers.frames_nvim, {
+            hl = { fg = '#5de4c7' },
+          }),
+          builtin.sections.buttons {
+            {
+              icon = '',
+              text = 'Find Files',
+              shortcut = 'f',
+              callback = function()
+                require('telescope.builtin').find_files()
+              end,
+            },
+            {
+              icon = '',
+              text = 'Find Word',
+              shortcut = 'w',
+              callback = function()
+                require('telescope.builtin').live_grep()
+              end,
+            },
+            {
+              icon = '',
+              text = 'Buffers',
+              shortcut = 'b',
+              callback = function()
+                require('telescope.builtin').buffers()
+              end,
+            },
+            {
+              icon = '',
+              text = 'Projects',
+              shortcut = 'p',
+              callback = function()
+                require('telescope').extensions.projects.projects()
+              end,
+            },
+          },
+          builtin.sections.oldfiles(),
+        },
+        mappings = {},
+        startup = true,
+        listed = false,
+      }
+    end,
     enabled = not vim.g.vscode,
   },
 
