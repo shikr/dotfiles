8e59aadcbbd607c99c52c28bd40d2d6bba8c025e
shikr <cristobal.rod0907@gmail.com>
2024-01-09 22:43:02 -0600

    fix: open veil only when there is no more buffers

--- nvchad/custom/configs/bufferline.lua
+++ nvchad/custom/configs/bufferline.lua
@@ -1,7 +1,18 @@
+local function has_bufnr(elements, bufnr)
+  for _, element in ipairs(elements) do
+    if bufnr == element.id then
+      return true
+    end
+  end
+
+  return false
+end
+
 local function bufdelete(bufnr)
   local elements = require('bufferline').get_elements().elements
+
   if #elements >= 1 then
-    if #elements == 1 then
+    if #elements == 1 and has_bufnr(elements, bufnr) then
       vim.cmd 'Veil'
     end
     vim.cmd('bd! ' .. bufnr)
