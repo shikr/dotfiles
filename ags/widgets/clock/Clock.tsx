eec5b41c3e8e3e442550765a52b081d8704c36e3
shikr <cristobal.rod0907@gmail.com>
2025-07-03 19:05:42 -0600

    feat: use clock-format setting in clock module

--- ags/widgets/clock/Clock.tsx
+++ ags/widgets/clock/Clock.tsx
@@ -1,10 +1,34 @@
+import { createSettings, createState } from 'ags';
 import { Gtk } from 'ags/gtk4';
+import { exec } from 'ags/process';
 import { createPoll } from 'ags/time';
+import Gio from 'gi://Gio';
+
+const TIME_FORMAT = {
+    '24h': '%R',
+    '12h': '%I:%M %p',
+};
+
+type TimeKey = keyof typeof TIME_FORMAT;
+
+const getFormat = (format: string) => [
+    'date',
+    '+' +
+        (format in TIME_FORMAT
+            ? TIME_FORMAT[format as TimeKey]
+            : TIME_FORMAT['12h']),
+];
 
 function Clock() {
+    const gsettings = Gio.Settings.new('org.gnome.desktop.interface');
+    const { clockFormat } = createSettings(gsettings, {
+        'clock-format': 's',
+    });
     const date = createPoll('', 1000, ['date', '+%d/%m/%y']);
-    const time = createPoll('', 1000, ['date', '+%I:%M %p']);
     const tooltip = createPoll('', 1000, ['date', '+%A %d %B %Y']);
+    const [time, setTime] = createState(exec(getFormat(clockFormat.get())));
+
+    setInterval(() => setTime(exec(getFormat(clockFormat.get()))), 1000);
 
     return (
         <box
