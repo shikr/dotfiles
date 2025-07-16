ffd57754b29828149b671357cc08f60d1e793e13
shikr <cristobal.rod0907@gmail.com>
2025-07-16 14:20:00 -0600

    feat: add shutdown/reboot actions to app

--- ags/app.ts
+++ ags/app.ts
@@ -1,4 +1,6 @@
 import app from 'ags/gtk4/app';
+import { execAsync } from 'ags/process';
+import Gio from 'gi://Gio';
 import style from './style.scss';
 import Bar from './widgets/Bar';
 import NotificationWindow from './widgets/notification/NotificationWindow';
@@ -8,6 +10,15 @@ const windows = [Bar, NotificationWindow];
 app.start({
     css: style,
     main() {
+        const shutdownAction = new Gio.SimpleAction({ name: 'shutdown' });
+        const rebootAction = new Gio.SimpleAction({ name: 'reboot' });
+
+        shutdownAction.connect('activate', () => execAsync('poweroff'));
+        rebootAction.connect('activate', () => execAsync('reboot'));
+
+        app.add_action(shutdownAction);
+        app.add_action(rebootAction);
+
         app.get_monitors().forEach(m => windows.forEach(win => win(m)));
     },
 });
