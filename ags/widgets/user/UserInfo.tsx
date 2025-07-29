42220d1718f1401466e896a1735c29cd93c244f0
shikr <cristobal.rod0907@gmail.com>
2025-07-28 19:03:50 -0600

    feat: add a menu to update user info

--- ags/widgets/user/UserInfo.tsx
+++ ags/widgets/user/UserInfo.tsx
@@ -1,5 +1,7 @@
-import { createState } from 'ags';
+import { createState, With } from 'ags';
+import { Gtk } from 'ags/gtk4';
 import AccountsService from 'gi://AccountsService';
+import Gio from 'gi://Gio';
 import GLib from 'gi://GLib';
 import Avatar from './Avatar';
 
@@ -12,18 +14,112 @@ function UserInfo() {
     const [displayName, setDisplayName] = createState(
         user.realName || username
     );
+    const [icon, setIcon] = createState(user.iconFile);
+    const [textName, setTextName] = createState(user.realName);
 
-    user.connect('changed', () =>
-        setDisplayName(user.realName || user.userName)
+    const imageFilter = Gtk.FileFilter.new();
+    imageFilter.add_mime_type('image/png');
+    imageFilter.add_mime_type('image/jpeg');
+
+    const dialog = new Gtk.FileDialog({
+        title: 'Select your avatar',
+        filters: Gtk.FilterListModel.new(null, imageFilter),
+    });
+
+    Gio._promisify(Gtk.FileDialog.prototype, 'open', 'open_finish');
+
+    const avatarOptions = [
+        {
+            label: 'Select',
+            action: () =>
+                dialog
+                    .open(null, null)
+                    .then(file =>
+                        user.set_icon_file(file?.get_path() ?? user.iconFile)
+                    )
+                    .catch((err: Gtk.DialogError) =>
+                        err.code !== 2 ? console.error(err) : err
+                    ),
+        },
+        {
+            label: 'Remove',
+            action: () => user.set_icon_file(''),
+        },
+    ];
+
+    user.connect(
+        'changed',
+        () => (
+            setDisplayName(user.realName || user.userName),
+            setIcon(user.iconFile)
+        )
     );
 
     return (
-        <button class="flat">
+        <menubutton class="flat" onMap={() => setTextName(user.realName)}>
             <box class="horizontal" spacing={6}>
                 <Avatar user={user} displayName={displayName} />
                 <label class="body" label={displayName} />
             </box>
-        </button>
+            <popover>
+                <box class="horizontal" spacing={6}>
+                    <menubutton>
+                        <With value={icon}>
+                            {(file: string) =>
+                                Boolean(file) &&
+                                GLib.file_test(file, GLib.FileTest.EXISTS) ? (
+                                    <image pixelSize={64} file={file} />
+                                ) : (
+                                    <image
+                                        pixelSize={64}
+                                        iconName="avatar-default-symbolic"
+                                    />
+                                )
+                            }
+                        </With>
+                        <popover>
+                            <Gtk.ListBox
+                                selectionMode={Gtk.SelectionMode.NONE}
+                                class="boxed-list"
+                                onRowActivated={(_, row) => {
+                                    if (row !== null)
+                                        avatarOptions[row.get_index()].action();
+                                }}
+                            >
+                                {avatarOptions.map(({ label }) => (
+                                    <label label={label} />
+                                ))}
+                            </Gtk.ListBox>
+                        </popover>
+                    </menubutton>
+                    <box
+                        class="vertical"
+                        valign={Gtk.Align.CENTER}
+                        orientation={Gtk.Orientation.VERTICAL}
+                    >
+                        <label
+                            class="heading"
+                            label="Real Name:"
+                            halign={Gtk.Align.START}
+                        />
+                        <box class="horizontal" spacing={6}>
+                            <entry
+                                text={textName}
+                                onNotifyText={source =>
+                                    setTextName(source.text)
+                                }
+                            />
+                            <button
+                                label="Apply"
+                                onClicked={() =>
+                                    user.set_real_name(textName.get())
+                                }
+                            />
+                        </box>
+                    </box>
+                </box>
+            </popover>
+        </menubutton>
     );
 }
 
