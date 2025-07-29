9d007b438aa2c9d5bb4699d9df231d382e12f2f9
shikr <cristobal.rod0907@gmail.com>
2025-07-28 19:09:18 -0600

    fix: check if the avatar image exists

--- ags/widgets/user/Avatar.tsx
+++ ags/widgets/user/Avatar.tsx
@@ -3,6 +3,7 @@ import { Gtk } from 'ags/gtk4';
 import AccountsService from 'gi://AccountsService';
 import Adw from 'gi://Adw';
 import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
 
 interface Props {
     user: AccountsService.User;
@@ -14,15 +15,16 @@ function Avatar({ user, displayName }: Props) {
         <Adw.Avatar
             $={self => {
                 const setAvatar = (avatar: string) =>
-                    !Boolean(avatar)
-                        ? self.set_custom_image(null)
-                        : self.set_custom_image(
+                    Boolean(avatar) &&
+                    GLib.file_test(avatar, GLib.FileTest.EXISTS)
+                        ? self.set_custom_image(
                               Gtk.IconPaintable.new_for_file(
                                   Gio.File.new_for_path(avatar),
                                   24,
                                   1
                               )
-                          );
+                          )
+                        : self.set_custom_image(null);
                 setAvatar(user.iconFile);
 
                 user.connect('changed', () => setAvatar(user.iconFile));
